2e2e45acf6738b8de97e1bbd07ce264860dc09c2
Werlene Lima <werlenelima41@gmail.com>
2026-04-16 17:43:29 -0300

    corrigindo script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const projetos = [
-  {
-    nome: "Projeto 1",
-    descricao: "Meu primeiro projeto",
-    link: "https://github.com/"
-  },
-  {
-    nome: "Projeto 2",
-    descricao: "Outro projeto legal",
-    link: "https://github.com/"
-  }
+    {
+        nome: "Projeto 1",
+        descricao: "Meu primeiro projeto",
+        link: "https://github.com/werlenelima41-pixel/meu-portfolio"
+    },
+    {
+        nome: "Projeto 2",
+        descricao: "Outro projeto legal",
+        link: "https://github.com/werlenelima41-pixel/meu-portfolio"
+    }
 ];
 
 const lista = document.getElementById("lista-projetos");
 
 projetos.forEach(p => {
-  lista.innerHTML += `
+    lista.innerHTML += `
     <div class="projeto">
-      <a href="${p.link}" target="_blank">
-        <h3>${p.nome}</h3>
-      </a>
-      <p>${p.descricao}</p>
+        <a href="${p.link}" target="_blank">
+            <h3>${p.nome}</h3>
+        </a>
+        <p>${p.descricao}</p>
     </div>
-  `;
+    `;
 });
